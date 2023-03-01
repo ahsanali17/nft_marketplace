028b1dc04206090a6fc26e9940656077f6a75f4e
ahsanali17 <ahsantime1@gmail.com>
2023-02-28 19:55:21 -0500

    continous fixes

--- src/components/MyNFTCard/MyNFTCard.tsx
+++ src/components/MyNFTCard/MyNFTCard.tsx
@@ -1,46 +1,47 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
-import { EvmAddress } from '@moralisweb3/common-evm-utils';
+// import { EvmAddress, EvmNft } from '@moralisweb3/common-evm-utils';
 
 import images from '../../assets';
 
-type nftCardProps = {
-  nft: {
-    contractType: string,
-    tokenAddress: {
-      config: {
-        items: Record<string, unknown>
-      },
-      _value: string,
-    },
-    tokenId: string,
-    tokenUri: string,
-    metadata: {
-      name: string,
-      description: string,
-      image: string,
-      animation_url: string,
-      attributes: Record<string, unknown>[],
-    },
-    name: string,
-    symbol: string,
-    amount: number,
-    blockNumberMinted: string,
-    blockNumber: string,
-    ownerOf: {
-      config: {
-        items: Record<string, unknown>
-      },
-      _value: string,
-    },
-    tokenHash: string,
-  }
-  chain: string | undefined;
-  address: EvmAddress | string;
-}
+// type nftCardProps = {
+//   nft: {
+//     contractType: string,
+//     tokenAddress: {
+//       config: {
+//         items: Record<string, unknown>
+//       },
+//       _value: string,
+//     },
+//     tokenId: string,
+//     tokenUri: string,
+//     metadata: {
+//       name: string,
+//       description: string,
+//       image: string,
+//       animation_url: string,
+//       attributes: Record<string, unknown>[],
+//     },
+//     name: string,
+//     symbol: string,
+//     amount: number,
+//     blockNumberMinted: string,
+//     blockNumber: string,
+//     ownerOf: {
+//       config: {
+//         items: Record<string, unknown>
+//       },
+//       _value: string,
+//     },
+//     tokenHash: string,
+//   }
+//   chain: string | undefined;
+//   address: EvmAddress | string;
+// }
 
-const MyNFTCard = ({ nft }: nftCardProps) => {
+// Keeping it any for now since the type object is too large.
+const MyNFTCard = ({nft}: any) => {
  const { name, symbol,  } = nft;
 
  // const { data: nftMetadata } = useEvmContractNFTs({
@@ -57,7 +58,7 @@ const MyNFTCard = ({ nft }: nftCardProps) => {
   return (
    <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md">
      <div className="relative w-full h-52 sm:h-36 xs:h-56 mind:h-60 minlg:h-300 rounded-2xl overflow-hidden">
-       <Image alt="nft-placeholder" layout="fill" objectFit="cover" src={''} />
+       <Image alt="nft-placeholder" layout="fill" objectFit="cover" src={images.twitter} />
      </div>
 
      <div className="mt-3 flex flex-col">
